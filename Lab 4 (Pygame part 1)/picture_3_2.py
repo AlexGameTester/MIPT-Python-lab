5333be8689d25734e3146a8e9b95606463b3f427
Alexey Kirkoskii <kirkovskii.as@phystech,edu>
2020-10-08 22:49:47 +0300

    main() improvements
    Added docstring and comments
    Removed unused variable
    Applied auto-formatting to file

--- Lab 4 (Pygame part 1)/picture_3_2.py
+++ Lab 4 (Pygame part 1)/picture_3_2.py
@@ -4,14 +4,17 @@ import math
 
 
 def main():
+    """
+    Main method that initializes pygame and draws scene
+    """
+    # set up
     pyg.init()
-    FPS = 60
 
     screen_width, screen_height = 1032, 768
 
     screen = pyg.display.set_mode((screen_width, screen_height))
     screen.fill(COLOR['GREEN'])
-
+    # scene drawing
     draw_scene(screen, screen_width, screen_height)
 
     # display and quit event handling
@@ -47,9 +50,9 @@ def draw_scene(screen, screen_width, screen_height):
     line(screen, COLOR['BLACK'], (80, 510), (65, 300), 2)
     balloons(screen, 65, 300, 50)
 
-    line(screen, COLOR['BLACK'], (screen_width//2, 30 +
-                                  screen_height//2), (20+screen_width//2, screen_height//4), 2)
-    balloons(screen, 20+screen_width//2, screen_height//4, 60, size=120)
+    line(screen, COLOR['BLACK'], (screen_width // 2, 30 +
+                                  screen_height // 2), (20 + screen_width // 2, screen_height // 4), 2)
+    balloons(screen, 20 + screen_width // 2, screen_height // 4, 60, size=120)
 
     ice_cream(screen, 940, 500, 90, size=80)
 
@@ -66,37 +69,37 @@ def man(surface, x, y, width, height):
     ellipse(surface, COLOR['PURPLE'], torso_rect)
 
     # face
-    face_center = (x + width//2, y - height//6)
+    face_center = (x + width // 2, y - height // 6)
     face_height = height // 5
     circle(surface, COLOR['FACE_COLOR'], face_center, face_height)
     # eyes
-    eyes_height = y - int(0.22*height)
-    circle(surface, COLOR['WHITE'], (x+width//3, eyes_height), height//20)
+    eyes_height = y - int(0.22 * height)
+    circle(surface, COLOR['WHITE'], (x + width // 3, eyes_height), height // 20)
     circle(surface, COLOR['BLACK'],
-           (x + int(0.32*width), eyes_height), height//45)
+           (x + int(0.32 * width), eyes_height), height // 45)
     circle(surface, COLOR['WHITE'],
-           (x + int(0.65*width), eyes_height), height//20)
+           (x + int(0.65 * width), eyes_height), height // 20)
     circle(surface, COLOR['BLACK'],
-           (x + int(0.66*width), eyes_height), height//45)
+           (x + int(0.66 * width), eyes_height), height // 45)
     # nose
     line(surface, COLOR['BLACK'],
-         (x + width//2, y - int(0.2*height)),
-         (x + width//2, y - int(0.1*height)), 1)
+         (x + width // 2, y - int(0.2 * height)),
+         (x + width // 2, y - int(0.1 * height)), 1)
     # mouth
     arc(surface, COLOR['BLACK'],
-        (x + int(0.35*width), y - int(0.18*height), int(0.3*width), height//6),
+        (x + int(0.35 * width), y - int(0.18 * height), int(0.3 * width), height // 6),
         1.1 * math.pi, 1.9 * math.pi, 2)
     # hands
     line(surface, COLOR['BLACK'],
-         (x + width//5, y + height//10),
-         (x - width//2, y + int(0.65*height)), 2)
+         (x + width // 5, y + height // 10),
+         (x - width // 2, y + int(0.65 * height)), 2)
     line(surface, COLOR['BLACK'],
-         (x + int(.8*width), y + height//10),
-         (x + int(1.6*width), y + height//2), 2)
+         (x + int(.8 * width), y + height // 10),
+         (x + int(1.6 * width), y + height // 2), 2)
     # legs
-    leg1_top = (x + width//3, y + int(0.95*height))
-    leg1_mid = (x + width//20, y + int(1.5*height))
-    leg1_bot = (x - width//4, y + int(1.5*height) + 2)
+    leg1_top = (x + width // 3, y + int(0.95 * height))
+    leg1_mid = (x + width // 20, y + int(1.5 * height))
+    leg1_bot = (x - width // 4, y + int(1.5 * height) + 2)
     line(surface, COLOR['BLACK'],
          leg1_top,
          leg1_mid, 2)
@@ -104,9 +107,9 @@ def man(surface, x, y, width, height):
          leg1_mid,
          leg1_bot, 2)
 
-    leg2_top = (x + 2*width//3, y + int(0.95*height))
-    leg2_mid = (x + 3*width//4, y + int(1.49*height))
-    leg2_bot = (x + width, y + int(1.5*height))
+    leg2_top = (x + 2 * width // 3, y + int(0.95 * height))
+    leg2_mid = (x + 3 * width // 4, y + int(1.49 * height))
+    leg2_bot = (x + width, y + int(1.5 * height))
     line(surface, COLOR['BLACK'],
          leg2_top,
          leg2_mid, 2)
@@ -128,7 +131,7 @@ def woman(surface, x, y, width, height):
                                    y - height // 8, height // 2, height // 2))
     # torso
     polygon(surface, COLOR['PINK'],
-            [(x, y), (x-width//2, y+height), (x+width//2, y+height)])
+            [(x, y), (x - width // 2, y + height), (x + width // 2, y + height)])
     # head
     circle(surface, COLOR['FACE_COLOR'], (x, y - height // 16), 2 * height // 11)
     # eyes
@@ -152,14 +155,14 @@ def woman(surface, x, y, width, height):
         1.1 * math.pi, 1.9 * math.pi, 2)
     # hands
     line(surface, COLOR['BLACK'],
-         (x - width//12, y + height//6),
+         (x - width // 12, y + height // 6),
          (x - int(0.8 * width), y + int(0.55 * height)), 2)
     line(surface, COLOR['BLACK'],
-         (x + width//12, y + height//6),
+         (x + width // 12, y + height // 6),
          (x + int(0.4 * width), y + int(0.4 * height)), 2)
     line(surface, COLOR['BLACK'],
          (x + int(0.4 * width), y + int(0.4 * height)),
-         (x + int(0.6 * width), y + height//4), 2)
+         (x + int(0.6 * width), y + height // 4), 2)
     # legs
     #  leg 1
     leg1_mid = (x - width // 10, y + int(1.45 * height))
@@ -168,11 +171,11 @@ def woman(surface, x, y, width, height):
          leg1_mid, 2)
     line(surface, COLOR['BLACK'],
          leg1_mid,
-         (x - int(0.3*width), y + int(1.45*height)), 2)
+         (x - int(0.3 * width), y + int(1.45 * height)), 2)
     #  leg 2
     leg2_mid = (x + width // 10, y + int(1.45 * height))
     line(surface, COLOR['BLACK'],
-         (x + width//10, y + height),
+         (x + width // 10, y + height),
          leg2_mid, 2)
     line(surface, COLOR['BLACK'],
          leg2_mid,
@@ -221,8 +224,8 @@ def ice_cream(surface: pyg.Surface, x, y, angle, size):
                                      int(0.25 * triangle_left_y + 0.75 * triangle_right_y)), size // 4)
     circle(surface, COLOR['RED'], (int(0.75 * triangle_left_x + 0.25 * triangle_right_x),
                                    int(0.75 * triangle_left_y + 0.25 * triangle_right_y)), size // 4)
-    x_white = (triangle_left_x + triangle_right_x)/2 - size//4 * math.cos(math.radians(30 + angle))
-    y_white = (triangle_left_y + triangle_right_y)/2 - size//4 * math.sin(math.radians(30 + angle))
+    x_white = (triangle_left_x + triangle_right_x) / 2 - size // 4 * math.cos(math.radians(30 + angle))
+    y_white = (triangle_left_y + triangle_right_y) / 2 - size // 4 * math.sin(math.radians(30 + angle))
     circle(surface, COLOR['WHITE'], (int(x_white), int(y_white)), size // 4)
 
 
